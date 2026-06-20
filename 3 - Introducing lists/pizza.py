a151a76fc377fa7865e1dd77634fd829c416dc02
NataniarroM <nataniarro@hotmail.com>
2026-06-20 19:19:49 -0300

    More jobs with lists

---
+++ 3 - Introducing lists/pizza.py
@@ -0,0 +1,6 @@
+pizzas = ["Pepperoni", "Margherita", "Cheese"]
+
+for pizza in pizzas:
+    print("I like " + pizza + " pizza")
+
+print("I really love pizza")
---
+++ 3 - Introducing lists/seeing_world.py
@@ -0,0 +1,25 @@
+locations = ["Rome", "Bueno Aires", "Tokyo", "Paris", "Rio de Janeiro"]
+
+print(sorted(locations))
+print(locations)
+print("\n")
+
+print(sorted(locations, reverse=True))
+print(locations)
+print("\n")
+
+locations.reverse()
+print(locations)
+print("\n")
+
+locations.reverse()
+print(locations)
+print("\n")
+
+locations.sort()
+print(locations)
+print("\n")
+
+locations.sort(reverse=True)
+print(locations)
+print("\n")
