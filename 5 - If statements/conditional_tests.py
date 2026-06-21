541fe096fd8d651a8edc4af2e6f671558260554e
NataniarroM <nataniarro@hotmail.com>
2026-06-21 14:03:37 -0300

    starting conditional tests

---
+++ 5 - If statements/conditional_tests.py
@@ -0,0 +1,23 @@
+car = "BMW"
+print(car == "bmw")
+print(car.lower() == "bmw")
+
+print("\n")
+
+value = 5
+print(value <= 5)
+print(value < 5)
+print(value > 5)
+print(value >= 5)
+
+print("\n")
+
+value2 = 4
+print(value > 2 and value2 > 5)
+print(value > 2 or value2 > 5)
+
+print("\n")
+
+values = [value for value in range(1, 5)]
+print(5 not in values)
+print(4 in values)
