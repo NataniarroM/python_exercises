b737cdecb48a4807f096034dc7e31b567ef0dce5
NataniarroM <nataniarro@hotmail.com>
2026-06-22 16:43:51 -0300

    new conditional tests

---
+++ 5 - If statements/stages_of_life.py
@@ -0,0 +1,16 @@
+person_age = 7
+
+if 0 <= person_age < 2:
+    print("You are a baby")
+elif 4 > person_age >= 2:
+    print("You are a toddler")
+elif 13 > person_age >= 4:
+    print("You are a kid")
+elif 20 > person_age >= 13:
+    print("You are a teenager")
+elif 65 > person_age >= 20:
+    print("You are a adult")
+elif 120 > person_age >= 65:
+    print("You are a elder")
+else:
+    print("Invalid age")
